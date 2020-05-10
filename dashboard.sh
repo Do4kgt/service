bf1d544641e902414b8040dafb974705ed670ef9
Do4kgt <61664243+Do4kgt@users.noreply.github.com>
2020-05-10 20:22:35 +0200

    Update dashboard.sh

--- dashboard.sh
+++ dashboard.sh
@@ -6,23 +6,24 @@ sudo usermod -G www-data -a pi
 
 sudo chown -R www-data:www-data /var/www/html
 
-sudo chmod -R 775 /var/www/html
+sudo chmod -R 775 /var/www
 
-sudo apt-get install php7.0-common php7.0-cgi php
+sudo apt-get install php7.3-common php7.3-cgi php
 
-sudo lighty-enable-mod fastcgi
-
-sudo lighty-enable-mod fastcgi-php
-
-sudo service lighttpd force-reload
 cd /
 cd opt
 cd dmr
 sudo mkdir tmp
 cd tmp
 sudo git clone https://github.com/Do4kgt/html.git
-sudo chmod -R 777 /var/www/html
+sudo chmod -R 777 /var/www
 sudo rm /var/www/html/*
 sudo cp -r /opt/dmr/tmp/html/ /var/www
 sudo rm -r /opt/dmr/tmp/
 cd ..
+
+sudo lighty-enable-mod fastcgi
+
+sudo lighty-enable-mod fastcgi-php
+
+sudo service lighttpd force-reload
